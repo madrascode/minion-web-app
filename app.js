3459dbd1961f32e2733ea0184a144798aba157bd
dheeraj purohit <purohitdheeraj89@gmail.com>
2021-02-27 16:37:40 +0530

    button and text are events added

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-// input 
-var username = prompt('Give me username')
+// // input 
+// var username = prompt('Give me username')
 
-// processing
-var welcomeMessage = 'this script works!!' + username
+// // processing
+// var welcomeMessage = 'this script works!!' + username
 
-// output
-alert(welcomeMessage)
+// // output
+// alert(welcomeMessage)
+
+
+var btnClicked = document.querySelector('#btn-translate')
+var textInput = document.querySelector(".text-input");
+
+console.log(textInput)
+function btnKliked(){
+    console.log('clicked!')
+    console.log("input",textInput.value)
+}
+
+btnClicked.addEventListener('click', btnKliked)
